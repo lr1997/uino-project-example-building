939554b06ad7045affc290e19711d7844f0219d8
LR <cnvp21@gmail.com>
2025-03-31 15:07:13 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# uino-project-example-building
---
+++ src/index.js
@@ -0,0 +1,36 @@
+let campus = null;
+let building = null;
+
+const app = new THING.App({
+  url: "../campus/campus.gltf",
+  comlete: (e) => {
+    campus = e.object;
+    building = campus.query(".Building")[0];
+    createMarkerForDevice();
+  },
+});
+
+function createMarker(obj) {
+  const makerImg = new THING.ImageTexture("./images/mar_sensor.png");
+  new THING.Marker({
+    name: "car_marker",
+    parent: obj,
+    scale: [12, 12, 12],
+    localPosition: [0, 3, 0],
+    alwayOnTop: true,
+    style: {
+      image: makerImg,
+    },
+    visible: true,
+  });
+}
+
+function createMarkerForDevice() {
+  console.log("createMarkerForDevice");
+
+  const cars = app.query("tags:and(car)");
+  cars.forEach((device) => {
+    createMarker(device);
+    console.log(device);
+  });
+}
